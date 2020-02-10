6d177324ae71982e15c6fc673190dfbf00298a4d
Dmytro Hryhoruk <dimamyhgrygoruk@gmail.com>
2020-02-10 16:38:10 +0200

    renamed variables

--- src/main/java/Solution.java
+++ src/main/java/Solution.java
@@ -1,13 +1,13 @@
 import java.util.concurrent.ThreadLocalRandom;
 
 public class Solution {
-    public static final int LOWER_RANGE = 3;
-    public static final int UPPER_RANGE = 5;
+    public static final int ROWS = 3;
+    public static final int COLUMNS = 5;
     private static final int[][] ARRAY = new int[3][5];
 
     private static void fillingArrayWithZeros() {
-        for (int i = 0; i < ARRAY.length; i++) {
-            for (int j = 0; j < ARRAY[i].length; j++) {
+        for (int i = 0; i < ROWS; i++) {
+            for (int j = 0; j < COLUMNS; j++) {
                 ARRAY[i][j] = 0;
             }
         }
@@ -15,22 +15,21 @@ public class Solution {
 
     private static void replacingZeroesWithinRange() {
         int randomNum = ThreadLocalRandom.current()
-                .nextInt(LOWER_RANGE, UPPER_RANGE + 1);
+                .nextInt(ROWS, COLUMNS + 1);
         System.out.println("\n" + randomNum + " elements will be replaced by ones\n");
         for (int i = 0; i < randomNum; i++) {
-            int row = Integer.MIN_VALUE;
-            int column = Integer.MIN_VALUE;
+            int row, column;
             do {
-                row = ThreadLocalRandom.current().nextInt(0, ARRAY.length);
-                column = ThreadLocalRandom.current().nextInt(0, ARRAY[row].length);
+                row = ThreadLocalRandom.current().nextInt(0, ROWS);
+                column = ThreadLocalRandom.current().nextInt(0, COLUMNS);
             } while (ARRAY[row][column] != 0);
             ARRAY[row][column] = 1;
         }
     }
 
     private static void printArray() {
-        for (int i = 0; i < ARRAY.length; i++) {
-            for (int j = 0; j < ARRAY[i].length; j++) {
+        for (int i = 0; i < ROWS; i++) {
+            for (int j = 0; j < COLUMNS; j++) {
                 System.out.print(ARRAY[i][j] + " ");
             }
             System.out.print("\n");
